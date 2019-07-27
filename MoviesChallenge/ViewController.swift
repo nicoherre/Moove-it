c1cfe37080da3a5df4f0f25d4811587cab1f2579
Nicolas Herrera <nicolasmhb+reg@gmail.com>
2019-07-27 20:19:34 -0300

    Initial Commit

---
+++ MoviesChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesChallenge
+//
+//  Created by Nicolas Herrera on 7/27/19.
+//  Copyright © 2019 Nicolas Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
